fe1d22b45f81e33b4355fbc46aaf2f499ae9cd2e
charlie <jhdhj999@gmail.com>
2024-10-18 15:23:44 +0600

    Relationship between users and posts defined

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -9,20 +10,33 @@ class Post(models.Model):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
     # Field for the post title. It translates into VARCHAR
-    title = models.Charfield(max_length = 250)
+    title = models.CharField(max_length = 250)
+    
     # Short label
     slug = models.SlugField(max_length = 250)
+    
+    # Relationship betweeen users and posts
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
+    )
+    
     # Field for storing body of the post.
     body = models.TextField()
+    
     # Publish date and time of the post. It will be translated
     # into a DATETIME column in the sql database
     publish = models.DateTimeField(default=timezone.now)
+    
     # Store the date and time when the post was created.
     # Because of auto_now_add, the date will be saved automatically
     # when creating an object.
     created = models.DateTimeField(auto_now_add=True)
+    
     # Store when the post was updated.
     updated = models.DateTimeField(auto_now=True)
+    
 class Meta:
     # This class defines metadata for the model.
     # Ordering is used to sort results by publish field.
@@ -31,6 +45,7 @@ class Meta:
     indexes = [
         models.Index(fields=['-publish']),
     ]
+    
 # Default python method to return a string with human-readable
 # representation of the object. It will be used for displaying 
 # the name of the object in places, such as, django admin site
