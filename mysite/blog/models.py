3813f86ce11585b230d0b5419d66d7df2b918efb
charlie <jhdhj999@gmail.com>
2024-10-26 22:01:40 +0600

    Created list and detail views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return(
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -42,6 +48,8 @@ class Post(models.Model):
         choices=Status,
         default=Status.DRAFT
     )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
     
     class Meta:
         # This class defines metadata for the model.
