2c9c8b680594fbe3ea07db2292c06605da4233fc
charlie <jhdhj999@gmail.com>
2024-10-27 21:50:57 +0600

    Implemented class-based view to list posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,10 @@ from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from .models import Post
 #from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 # Create your views here.
+
+
 
 def post_list(request):
     post_list = Post.published.all()
@@ -41,3 +44,12 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
         )
+    
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
