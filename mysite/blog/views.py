248e33d7047d49aafff97e596a18939fc53f2085
charlie <jhdhj999@gmail.com>
2024-10-29 19:47:14 +0600

    Bugs fixed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,11 @@
-from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
-from .models import Post
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import ListView
-from django.views.decorators.http import require_POST
-from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
-# Create your views here.
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView
 
+from .forms import CommentForm, EmailPostForm
+from .models import Post
 
 
 def post_list(request):
@@ -20,15 +19,16 @@ def post_list(request):
         # If page_number is not an integer get the first page
         posts = paginator.page(1)
     except EmptyPage:
-            # If page_number is out of range get last page of results
-            posts = paginator.page(paginator.num_pages)
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
-    
-def post_detail(request, year, month, day, post):    
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
@@ -41,7 +41,7 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
-    
+
     return render(
         request,
         'blog/post/detail.html',
@@ -49,19 +49,21 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'form': form
-            
-         }
-        )
-    
+        },
+    )
+
+
 class PostListView(ListView):
     """
     Alternative post list view
     """
+
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-    
+
+
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(
@@ -70,14 +72,13 @@ def post_share(request, post_id):
         status=Post.Status.PUBLISHED
     )
     sent = False
-    
+
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
             post_url = request.build_absolute_uri(
                 post.get_absolute_url()
             )
@@ -87,7 +88,7 @@ def post_share(request, post_id):
             )
             message = (
                 f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
             send_mail(
                 subject=subject,
@@ -96,19 +97,20 @@ def post_share(request, post_id):
                 recipient_list=[cd['to']],
             )
             sent = True
-        else:
-            form = EmailPostForm()
-        return render(
-            request,
-            'blog/post/share.html',
-            {
-                'post': post,
-                'form': form,
-                'sent': sent
-            }
-        )
-    
-    
+
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        },
+    )
+
+
 @require_POST
 def post_comment(request, post_id):
     post = get_object_or_404(
@@ -117,13 +119,12 @@ def post_comment(request, post_id):
         status=Post.Status.PUBLISHED
     )
     comment = None
-
     # A comment was posted
     form = CommentForm(data=request.POST)
     if form.is_valid():
         # Create a Comment object without saving it to the database
         comment = form.save(commit=False)
-        # Assign the post to the ocmment
+        # Assign the post to the comment
         comment.post = post
         # Save the comment to the database
         comment.save()
@@ -135,5 +136,4 @@ def post_comment(request, post_id):
             'form': form,
             'comment': comment
         },
-        )
-    
+    )
