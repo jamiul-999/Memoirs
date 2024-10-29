b8c628a0dbfd9ef31029fe7ef03155671ca9e4a0
charlie <jhdhj999@gmail.com>
2024-10-29 19:17:34 +0600

    Added share and comment features

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from .models import Post
-#from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 # Create your views here.
 
 
@@ -27,48 +28,29 @@ def post_list(request):
         {'posts': posts}
     )
     
-def post_detail(request, year, month, day, post):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("No Post found.")
+def post_detail(request, year, month, day, post):    
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day
+        publish__day=day,
     )
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
-        )
-    
-def post_share(request, post_id):
-    # Retrieve post by id
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
-    if request.method == 'POST':
-        # Form was submitted
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            # Form fields passed validation
-            cd = form.cleaned_data
-            # ... send email
-        else:
-            form = EmailPostForm()
-        return render(
-            request,
-            'blog/post/share.html',
-            {
-                'post': post,
-                'form': form
-            }
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+            
+         }
         )
     
 class PostListView(ListView):
@@ -79,3 +61,79 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+    
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent = False
+    
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']],
+            )
+            sent = True
+        else:
+            form = EmailPostForm()
+        return render(
+            request,
+            'blog/post/share.html',
+            {
+                'post': post,
+                'form': form,
+                'sent': sent
+            }
+        )
+    
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the ocmment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        },
+        )
+    
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+from decouple import config
+# ...
+# Email server configuration
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+#Email_BACKEND = 'django.core.mail.backends.console.EmailBackend'
