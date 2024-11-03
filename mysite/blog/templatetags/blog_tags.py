ff9a5b53036750aee64e77147d9a6430c053764e
charlie <jhdhj999@gmail.com>
2024-11-03 14:23:02 +0600

    Support for markdown syntax added

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
+import markdown
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+
 register = template.Library()
 
 @register.simple_tag
@@ -17,3 +20,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))    
