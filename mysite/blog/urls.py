0018da2e93d314d5b3666730d9643b4aaaa31787
charlie <jhdhj999@gmail.com>
2024-11-04 01:20:14 +0600

    Feeds created for blog posts

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from .feeds import LatestPostsFeed
+
 app_name = 'blog'
 
 urlpatterns = [
@@ -16,5 +18,6 @@ urlpatterns = [
     ),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
     
 ]
