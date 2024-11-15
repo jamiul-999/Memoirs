98b65c632808b1e32428757ce9fa564a4d7950d0
charlie <jhdhj999@gmail.com>
2024-11-16 02:07:58 +0600

    Stemming and ranking results search feature added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,7 +5,11 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 
@@ -166,11 +170,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body'),
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=query)
+                .filter(search=search_query)
+                .order_by('-rank')
             )
     return render(
         request,
